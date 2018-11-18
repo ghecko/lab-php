1c36d30c917d76f11a70f99ef431fc7e02ed17c6
ghecko <ovre.jordan@gmail.com>
2018-11-18 23:16:01 +0100

    change bootstrap style import

--- blog-tree.php
+++ blog-tree.php
@@ -8,29 +8,29 @@
     <link rel="stylesheet" href="style/main.css">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="/vendor/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/vendor/AdminLTE/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/AdminLTE/bower_components/font-awesome/css/font-awesome.min.css">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="/vendor/AdminLTE/bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="vendor/AdminLTE/bower_components/Ionicons/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="/vendor/AdminLTE/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="vendor/AdminLTE/dist/css/AdminLTE.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="/vendor/AdminLTE/dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="vendor/AdminLTE/dist/css/skins/_all-skins.min.css">
 
     <!-- jQuery 3 -->
-    <script src="/vendor/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="vendor/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap 3.3.7 -->
-    <script src="/vendor/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="vendor/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- SlimScroll -->
-    <script src="/vendor/AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="vendor/AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
     <!-- FastClick -->
-    <script src="/vendor/AdminLTE/bower_components/fastclick/lib/fastclick.js"></script>
+    <script src="vendor/AdminLTE/bower_components/fastclick/lib/fastclick.js"></script>
     <!-- AdminLTE App -->
-    <script src="/vendor/AdminLTE/dist/js/adminlte.min.js"></script>
+    <script src="vendor/AdminLTE/dist/js/adminlte.min.js"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="/vendor/AdminLTE/dist/js/demo.js"></script>
+    <script src="vendor/AdminLTE/dist/js/demo.js"></script>
     <!-- page script -->
 </head>
 <body>
