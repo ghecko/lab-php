4e14803b9e70d94fdc38c0d46ba899642be471d5
ghecko <ovre.jordan@gmail.com>
2018-11-18 16:44:41 +0100

    password not hashed

--- classes/class.password.php
+++ classes/class.password.php
@@ -192,22 +192,13 @@ if (!defined('PASSWORD_DEFAULT')) {
          *
          * @return boolean If the password matches the hash
          */
-        public function password_verify($password, $hash) {
-            if (!function_exists('crypt')) {
-                trigger_error("Crypt must be loaded for password_verify to function", E_USER_WARNING);
+        public function password_verify($password, $submit_password) {
+            if($password === $submit_password){
+                return true;
+            }
+            else{
                 return false;
             }
-            $ret = crypt($password, $hash);
-            if (!is_string($ret) || strlen($ret) != strlen($hash) || strlen($ret) <= 13) {
-                return false;
-            }
-
-            $status = 0;
-            for ($i = 0; $i < strlen($ret); $i++) {
-                $status |= (ord($ret[$i]) ^ ord($hash[$i]));
-            }
-
-            return $status === 0;
         }
 
     }
