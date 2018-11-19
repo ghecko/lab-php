6dd8c0393a8ce2399a7f64484f1df75d780c038a
ghecko <ovre.jordan@gmail.com>
2018-11-19 01:37:41 +0100

    change index page style

--- index.php
+++ index.php
@@ -47,25 +47,27 @@
             </section>
             <section class="content">
                 <div class="box box-default">
-                    <?php
-                        try {
+                    <div class="box-body">
+                        <?php
+                            try {
 
-                            $stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
-                            while($row = $stmt->fetch()){
+                                $stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+                                while($row = $stmt->fetch()){
 
-                                echo '<div class="panel box box-primary">';
-                                    echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-                                    echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-                                    echo '<p>'.$row['postDesc'].'</p>';
-                                    echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';
-                                echo '</div>';
+                                    echo '<div class="panel box box-primary">';
+                                        echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+                                        echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
+                                        echo '<p>'.$row['postDesc'].'</p>';
+                                        echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';
+                                    echo '</div>';
 
+                                }
+
+                            } catch(PDOException $e) {
+                                echo $e->getMessage();
                             }
-
-                        } catch(PDOException $e) {
-                            echo $e->getMessage();
-                        }
-                    ?>
+                        ?>
+                    </div>
 	            </div>
             </section>
         </div>
