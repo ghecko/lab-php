7e486ee8ae0290fe0ec0296816b73afbee790d5b
ghecko <ovre.jordan@gmail.com>
2018-11-19 01:36:03 +0100

    change index page style

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
                             $stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
                             while($row = $stmt->fetch()){
 
-                                echo '<div>';
+                                echo '<div class="panel box box-primary">';
                                     echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
                                     echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
                                     echo '<p>'.$row['postDesc'].'</p>';
