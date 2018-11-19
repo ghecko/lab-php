f858b235cf56ca92e3da9cb9a5bb000e456cc7f7
ghecko <ovre.jordan@gmail.com>
2018-11-19 13:38:08 +0100

    test image import from base64

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <!-- The user image in the navbar-->
                                 <?php
-                                echo "<img src=data:image/jpeg;base64,".base64_encode($_SESSION['picture'])." class='user-image' alt='User Image'>"
+                                    echo "<img src=data:image/jpeg;base64,".base64_encode($_SESSION['picture'])." class='user-image' alt='User Image'>";
                                 ?>
                                 <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
                                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
@@ -54,7 +54,7 @@
                                 <!-- The user image in the menu -->
                                 <li class="user-header">
                                     <?php
-                                        echo "<img src='data:image/jpeg;base64,".base64_encode($_SESSION['picture'])."' class='img-circle' alt='User Image'>"
+                                        echo "<img src='data:image/jpeg;base64,".base64_encode($_SESSION['picture'])."' class='img-circle' alt='User Image'>";
                                     ?>
                                     <p>
                                         <?php echo $_SESSION['username']?>
