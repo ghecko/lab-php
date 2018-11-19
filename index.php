e3e7a7c33aaa7d34bac936a69998c7a84e0d68b4
ghecko <ovre.jordan@gmail.com>
2018-11-19 01:32:00 +0100

    change index page style

--- index.php
+++ index.php
@@ -47,29 +47,29 @@
             </section>
             <section class="content">
                 <div class="box box-default">
+                    <?php
+                        try {
 
-		<?php
-			try {
+                            $stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+                            while($row = $stmt->fetch()){
 
-				$stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
-				while($row = $stmt->fetch()){
-					
-					echo '<div>';
-						echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-						echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-						echo '<p>'.$row['postDesc'].'</p>';				
-						echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';				
-					echo '</div>';
+                                echo '<div>';
+                                    echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+                                    echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
+                                    echo '<p>'.$row['postDesc'].'</p>';
+                                    echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';
+                                echo '</div>';
 
-				}
+                            }
 
-			} catch(PDOException $e) {
-			    echo $e->getMessage();
-			}
-		?>
-
-	</div>
-
-
+                        } catch(PDOException $e) {
+                            echo $e->getMessage();
+                        }
+                    ?>
+	            </div>
+            </section>
+        </div>
+    </div>
+</div>
 </body>
 </html>
