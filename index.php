dffca07835c3fcb359d74ceba3dd303d34d45092
ghecko <ovre.jordan@gmail.com>
2018-11-19 01:30:20 +0100

    change index page style

--- index.php
+++ index.php
@@ -1,18 +1,52 @@
 <?php require('includes/config.php'); ?>
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Blog</title>
-    <link rel="stylesheet" href="style/normalize.css">
-    <link rel="stylesheet" href="style/main.css">
+    <title>Stellar blog</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="vendor/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="vendor/AdminLTE/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="vendor/AdminLTE/bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="vendor/AdminLTE/dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="vendor/AdminLTE/dist/css/skins/_all-skins.min.css">
 </head>
-<body>
+<body class="hold-transition skin-blue layout-top-nav" style="height: auto; min-height: 100%;">
+<div class="wrapper">
+    <header class="main-header">
+        <nav class="navbar navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="/index.php" class="navbar-brand"><b>Stellar</b>Blog</a>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                </div>
 
-	<div id="wrapper">
-
-		<h1>Blog</h1>
-		<hr />
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="/index.php">Blog</a></li>
+                        <li class="active"><a href="/blog-tree.php">Vue d'ensemble</a></li>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+    </header>
+    <div class="content-wrapper">
+        <div class="container">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>Blog</h1>
+            </section>
+            <section class="content">
+                <div class="box box-default">
 
 		<?php
 			try {
