9feeae7b2a470f6909891060d942d75da0f84a41
ghecko <ovre.jordan@gmail.com>
2018-11-19 02:17:21 +0100

    test page include

--- index.php
+++ index.php
@@ -41,35 +41,13 @@
     </header>
     <div class="content-wrapper">
         <div class="container">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <h1>Blog</h1>
-            </section>
-            <section class="content">
-                <div class="box box-default">
-                    <div class="box-body">
-                        <?php
-                            try {
-
-                                $stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
-                                while($row = $stmt->fetch()){
-
-                                    echo '<div class="panel box box-primary">';
-                                        echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-                                        echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-                                        echo '<p>'.$row['postDesc'].'</p>';
-                                        echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';
-                                    echo '</div>';
-
-                                }
-
-                            } catch(PDOException $e) {
-                                echo $e->getMessage();
-                            }
-                        ?>
-                    </div>
-	            </div>
-            </section>
+            <?php
+                if(isset($_GET['page'])) {
+                    include($_GET['page'] . ".php");
+                } else {
+                    include('blog.php');
+                }
+            ?>
         </div>
     </div>
 </div>
